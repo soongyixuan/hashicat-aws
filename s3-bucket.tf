dd56dc765fde67d0a53193b7f8c4a1fc373e7985
Yi Xuan <soongyixuan@gmail.com>
2023-02-17 06:16:26 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,11 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/soongyixuan/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucket"
-  acl    = "private"
   bucket_prefix = var.prefix
-
-  versioning = {
-    enabled = true
-  }
 }
